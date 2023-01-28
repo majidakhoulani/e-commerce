bff186b2a1ad8fa923d341ccb9c4a124f2fe673a
majidakhoulani <majidakh1993@gmail.com>
2023-01-28 14:46:56 +0200

    Adding dist

---
+++ dist/assets/VContainer.4277dd71.js
@@ -0,0 +1,1 @@
+import"./VCard.ac925368.js";import{m as t,u as n}from"./router.168e54eb.js";import{i as r,c as s}from"./index.da3f8bea.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
