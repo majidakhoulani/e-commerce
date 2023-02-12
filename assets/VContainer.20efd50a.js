6e43f17e96e2d64b65dac8dd2b99bd6316481c87
majidakhoulani <majidakh1993@gmail.com>
2023-02-12 17:34:39 +0200

    adding dist after alter router

---
+++ assets/VContainer.20efd50a.js
@@ -0,0 +1,1 @@
+import"./VCard.860f20a7.js";import{m as t,u as n}from"./router.241d0a87.js";import{i as r,c as s}from"./index.32a880c0.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
