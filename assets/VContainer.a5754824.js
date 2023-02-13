20fd67c446f8033c2faf87fb032f5e9eb299c5e8
majidakhoulani <majidakh1993@gmail.com>
2023-02-13 15:39:45 +0200

    update 2 router

---
+++ assets/VContainer.a5754824.js
@@ -0,0 +1,1 @@
+import"./VCard.e5fec705.js";import{m as t,u as n}from"./router.c78ee979.js";import{i as r,c as s}from"./index.3c6b9447.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
