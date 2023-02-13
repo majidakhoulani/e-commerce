08a4fef3eba70123706436a8bdc8be550e49981c
majidakhoulani <majidakh1993@gmail.com>
2023-02-13 15:39:45 +0200

    update 2 router

---
+++ dist/assets/VSpacer.4377035e.js
@@ -0,0 +1,1 @@
+import{p as e}from"./router.c78ee979.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};
--- dist/assets/VSpacer.a1518168.js
+++
@@ -1,1 +0,0 @@
-import{p as e}from"./router.3ca15bff.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};
