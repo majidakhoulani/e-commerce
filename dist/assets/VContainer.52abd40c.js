80d9cc997597f67a179cbb6ec522f78bd4c2b310
majidakhoulani <majidakh1993@gmail.com>
2023-02-15 15:46:19 +0200

    update vit

--- dist/assets/VContainer.52abd40c.js
+++
@@ -1,1 +0,0 @@
-import"./VCard.b04bd65c.js";import{m as t,u as n}from"./router.c580d36b.js";import{i as r,c as s}from"./index.7a218b0c.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
--- dist/assets/VSpacer.14828508.js
+++
@@ -1,1 +0,0 @@
-import{p as e}from"./router.c580d36b.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};
