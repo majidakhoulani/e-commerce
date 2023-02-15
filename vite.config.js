fc164b838f367d2b976c6141addbd2a6595ebf1e
majidakhoulani <majidakh1993@gmail.com>
2023-02-15 17:13:35 +0200

    update vit & vue

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:process.env.NODE_ENV === "production" ? "/e-commerce/" : "/",
+  //base:process.env.NODE_ENV === "production" ? "/e-commerce/" : "/",
+  base: process.env.NODE_ENV === 'development' ? '/e-commerce/dev/baseurl/' : '/e-commerce/prod/baseurl/',
   plugins: [
     vue({
       template: { transformAssetUrls }
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/e-commerce/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/e-commerce/" : "/",
+  publicPath: process.env.NODE_ENV === 'development' ? '/e-commerce/dev/baseurl/' : '/e-commerce/prod/baseurl/'
 };
