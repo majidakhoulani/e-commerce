5a80fd39bb4bcfc81a5b97bd00d226ad122710e6
majidakhoulani <majidakh1993@gmail.com>
2023-02-15 14:39:12 +0200

    delete slash before paths in router

--- assets/VContainer.69f39951.js
+++
@@ -1,1 +0,0 @@
-import"./VCard.32e2723b.js";import{m as t,u as n}from"./router.0f5691bf.js";import{i as r,c as s}from"./index.c425251d.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
