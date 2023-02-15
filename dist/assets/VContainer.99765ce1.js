d28ea8303bab22ebb111e31349c1600b8a0d8c96
majidakhoulani <majidakh1993@gmail.com>
2023-02-15 11:23:21 +0200

    update navbar adding named routes

--- dist/assets/VContainer.99765ce1.js
+++
@@ -1,1 +0,0 @@
-import"./VCard.656bd634.js";import{m as t,u as n}from"./router.be1fc3b8.js";import{i as r,c as s}from"./index.ab496c87.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
