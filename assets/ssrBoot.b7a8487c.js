cc3685f02ff33e247b79afe41f4ffdacc91577f4
majidakhoulani <majidakh1993@gmail.com>
2023-02-17 15:21:33 +0200

    replace name images

--- assets/ssrBoot.b7a8487c.js
+++
@@ -1,1 +0,0 @@
-import{r as t,a7 as e,b as s,M as n}from"./index.3324e2ca.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
