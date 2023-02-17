cffc6b0c45e4e4d0487ac1653cf24351229ba4d1
majidakhoulani <majidakh1993@gmail.com>
2023-02-17 15:58:40 +0200

    update path

--- assets/VContainer.a687d299.js
+++
@@ -1,1 +0,0 @@
-import"./VCard.b66f9811.js";import{m as t,u as n}from"./router.7914750c.js";import{i as r,c as s}from"./index.e549fc29.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
