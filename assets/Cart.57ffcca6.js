07b73af6b057bfb342453552f3c8bb91689da5c9
majidakhoulani <majidakh1993@gmail.com>
2023-02-19 11:13:37 +0200

    alter paths

--- assets/Cart.57ffcca6.js
+++
@@ -1,1 +0,0 @@
-import{ad as e}from"./index.4b0aeb21.js";const r=e("Cart",{state:()=>({items:[]}),persist:!0,getters:{itemsCount(t){return t.items.length}},actions:{addToCard(t){console.log(t),this.items.push(t)},removeItem(t){this.items.splice(t,1)}}});export{r as u};
