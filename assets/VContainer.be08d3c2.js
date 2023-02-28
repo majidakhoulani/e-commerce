b87d437d0f46900cabf60f7e3392080b0b97e015
majidakhoulani <majidakh1993@gmail.com>
2023-02-28 18:09:45 +0200

    upadate7 paths team images

---
+++ assets/VContainer.be08d3c2.js
@@ -0,0 +1,1 @@
+import"./VCard.0d979f38.js";import{m as t,u as n}from"./router.110bcfe8.js";import{i as r,c as s}from"./index.15adaf46.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
