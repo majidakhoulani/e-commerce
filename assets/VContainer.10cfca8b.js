2e7354af9624ce5df2dd374ac4f7f63ff49fa181
majidakhoulani <majidakh1993@gmail.com>
2023-02-28 16:28:58 +0200

    update team view

---
+++ assets/VContainer.10cfca8b.js
@@ -0,0 +1,1 @@
+import"./VCard.cbdb3a43.js";import{m as t,u as n}from"./router.fbbb07a0.js";import{i as r,c as s}from"./index.c809fad4.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
---
+++ assets/VSpacer.01a9bf8b.js
@@ -0,0 +1,1 @@
+import{p as e}from"./router.fbbb07a0.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};
