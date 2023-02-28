fd81538375b73a5eb0f41eef23fbc55cee1460cb
majidakhoulani <majidakh1993@gmail.com>
2023-02-28 17:34:26 +0200

    upadate6 moving  team images into assets

---
+++ assets/VContainer.6a8cb743.js
@@ -0,0 +1,1 @@
+import"./VCard.c82b6545.js";import{m as t,u as n}from"./router.d8383798.js";import{i as r,c as s}from"./index.729f5071.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
---
+++ assets/VSpacer.f8ab6bb4.js
@@ -0,0 +1,1 @@
+import{p as e}from"./router.d8383798.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};
