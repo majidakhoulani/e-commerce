b1398b431847af64dfaad6ee42f26fdc16bfcda7
majidakhoulani <majidakh1993@gmail.com>
2023-02-28 17:19:23 +0200

    upadate4 paths team images

---
+++ assets/VContainer.ff3b90af.js
@@ -0,0 +1,1 @@
+import"./VCard.86a27686.js";import{m as t,u as n}from"./router.004bd479.js";import{i as r,c as s}from"./index.d68a87da.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
---
+++ assets/VSpacer.b8cd6ff9.js
@@ -0,0 +1,1 @@
+import{p as e}from"./router.004bd479.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};
