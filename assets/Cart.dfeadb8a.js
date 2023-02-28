9e65e571fd97ffc70a38c37e0096f76e20e94de0
majidakhoulani <majidakh1993@gmail.com>
2023-02-28 17:16:24 +0200

    upadate3 paths team images

---
+++ assets/Cart.dfeadb8a.js
@@ -0,0 +1,1 @@
+import{ad as e}from"./index.6c3dca50.js";const r=e("Cart",{state:()=>({items:[]}),persist:!0,getters:{itemsCount(t){return t.items.length}},actions:{addToCard(t){console.log(t),this.items.push(t)},removeItem(t){this.items.splice(t,1)}}});export{r as u};
