51d5d8bd814a0d96475a75304fcd851d1f25ac24
majidakhoulani <majidakh1993@gmail.com>
2023-03-02 15:39:49 +0200

    update11 team images path

--- assets/VContainer.5a4a4c7d.js
+++
@@ -1,1 +0,0 @@
-import"./VCard.c51431c0.js";import{m as t,u as n}from"./router.cfd3625b.js";import{i as r,c as s}from"./index.48afb60f.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
