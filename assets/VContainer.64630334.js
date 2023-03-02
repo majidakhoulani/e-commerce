1a706389cb46cb5154d91633b829f96a389bf2ca
majidakhoulani <majidakh1993@gmail.com>
2023-03-02 16:21:40 +0200

    update portfoilo & hero section

--- assets/VContainer.64630334.js
+++
@@ -1,1 +0,0 @@
-import"./VCard.8360a48a.js";import{m as t,u as n}from"./router.7c79b906.js";import{i as r,c as s}from"./index.bedbd450.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
--- assets/VSpacer.d1b8dcab.js
+++
@@ -1,1 +0,0 @@
-import{p as e}from"./router.7c79b906.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};
